606f71c3f99005f4478b8e87a3c0636b2a6696a2
Mahbub Ulit Tamam <43460315+mahbubulittamam@users.noreply.github.com>
2018-09-22 16:58:58 +0700

    Update MainActivity.java

--- app/src/main/java/com/kitkat/ulil/pindahhalamanandroidbyulil/MainActivity.java
+++ app/src/main/java/com/kitkat/ulil/pindahhalamanandroidbyulil/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected{
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView textview = new TextView(MainActivity.this);
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    } void onCreate(Bundle savedInstanceState)
+    }
 
 
     public void intent (View view) {
